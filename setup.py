e018dffb7da70ab69ada12cbe29e4ec1ab14301c
battih_git <huzefabattiwala.hb@gmail.com>
2025-12-25 19:35:48 +0530

    Requirements Added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='us_visa',
+    version='0.0.0'
+    author='Huzefa',
+    author_email='huzefabattiwala.hb@gmail.com',
+    packages= find_packages() 
+)
